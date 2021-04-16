cd0e55edd92ff17d762589eb1e4cd138ba0018a7
natalia <info.golpedehorno@gmail.com>
2021-04-16 17:49:58 -0300

    falta estilo en document write

---
+++ js/recetario.js
@@ -0,0 +1,5 @@
+//pedir 3 ingredientes 
+
+var ingrediente1 = prompt("Ingrese el PRIMER ingrediente para su receta")
+var ingrediente2 = prompt("Ingrese el SEGUNDO ingrediente para su receta")
+var ingrediente3 = prompt("Ingrese el TERCER ingrediente para su receta")
